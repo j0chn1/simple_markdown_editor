5d94c75b864dbe0ca52150f1c6c8f6a5323b56b4
j0chn1 <87828588+j0chn1@users.noreply.github.com>
2025-11-01 21:34:53 +0100

    Update markdown_syntax.dart

--- lib/src/markdown_syntax.dart
+++ lib/src/markdown_syntax.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:simple_markdown_editor/widgets/markdown_parse.dart';
 import 'package:markdown/markdown.dart' as md;
 
